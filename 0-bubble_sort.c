21d80f511c9328222e5aa6f6e99e9a9f1f6c200d
Shewanji <marrionwanjiru26@gmail.com>
2023-07-21 13:28:25 -0700

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -29,10 +29,9 @@ void bubble_sort(int *array, size_t size)
 				array[j + 1] = temp;
 
 				swapped = 1;
+				print_array(array, size);
 			}
 		}
-
-		print_array(array, size);
 
 		if (swapped == 0)
 			break;
